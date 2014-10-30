1cd2655c176f9e8499e0b8c71ec6c9e19433d751
amaury lavieille <amaury.lavieille@gmail.com>
2014-10-30 19:53:17 +0100

    add relation left bar

--- assets/js/article.js
+++ assets/js/article.js
@@ -7,7 +7,45 @@ $(document).ready(function() {
 
 	$('article .article-content a[href^="##refWiki##"]').click(getInformationDbpedia);
 	$('article .article-content a[href^="##refInner##"]').click(getInformationInner);
+	createLinkSideBarRelation();
 });
+
+
+var createLinkSideBarRelation = function(){
+
+	var linksRefInner = $('article .article-content a[href^="##refInner##"]');
+	var linksRefWiki = $('article .article-content a[href^="##refWiki##"]');
+	var content = "";
+	if(linksRefInner.length > 0 ){
+		content += "<h5 class='text-center'>Relations interne</h5>";
+		content += createListRefHtml(linksRefInner);
+	}
+	if(linksRefWiki.length > 0 ){
+		content += "<h5 class='text-center'>Relations wikipedia</h5>";
+		content += createListRefHtml(linksRefWiki);
+	}
+	
+	$(".list-relations").html(content);
+}
+
+
+var createListRefHtml = function(listLink){
+	
+	res = "<ul>";
+	listLink.each(function(index,value){
+		var href = $(value).attr("href");
+		var content = $(value).text();
+		if(href.indexOf("##refWiki##") == 0)
+			href = href.replace("##refWiki##", "");
+		else {
+			href = href.replace("##refInner##", "");
+			href = "/article/"+href;
+		}
+		res += "<li> <a href='"+href+"''>"+content+"</a></li>";
+	});
+	res += "</ul>";
+	return res;
+}
 
 var getInformationInner = function(){
 	var uri = $(this).attr("href").replace("##refInner##", "");
